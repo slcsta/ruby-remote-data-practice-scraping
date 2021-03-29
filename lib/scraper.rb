0c2d03f18e0b3c7b7bf99460fae56cf1448632c0
Sarah <sarahlchavez@gmail.com>
2021-03-29 13:12:51 -0700

    commit tree f17c371b2dfe55cf28a2a60bfeba9f3a7adbfc2e

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".site-header__hero__headline").text.strip
